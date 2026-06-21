8e40f1bed2d308b37562fdebeacf1ec818fa41fc
1DaviSLima <davi.lima8589@gmail.com>
2026-06-21 20:47:40 -0300

    feat:adicionar implementacao de link em imagem

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,21 +1,28 @@
 import Menu from "../components/Menu/index";
 import DaviReactLogo from "../assets/DaviReactLogo.png";
 import "./header.css";
+import { Link } from "react-router-dom";
+
 function Header() {
-    
-    return (
-        <header className="header-base">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-10">
-                        <img src={DaviReactLogo} alt="##Logomarca" className="header-img"/>
-                    </div>
-                    <div className="col-2">
-                        <Menu/>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="header-base">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-10">
+            <Link to={"/"}>
+              <img
+                src={DaviReactLogo}
+                alt="##Logomarca"
+                className="header-img"
+              />
+            </Link>
+          </div>
+          <div className="col-2">
+            <Menu />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
-export default Header
+export default Header;
