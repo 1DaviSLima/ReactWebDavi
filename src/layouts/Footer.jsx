0168c50a585347c0a618705c1db6ca6727787866
1DaviSLima <davi.lima8589@gmail.com>
2026-06-29 21:29:09 -0300

    style:adicionar estilo a componentes e layouts

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,27 +1,13 @@
 import "../components/css_Components/menu.css";
+import "./footer.css";
 function Footer() {
-  const style = {
-    color: "white",
-    backgroundColor: "rgb(25, 54, 79)",
-    padding: "10px",
-    display: "flex",
-    flexDirection: "column",
-    height: "200px",
-    justifyContent: "center",
-  };
-  const linkStyle = {
-  textDecoration: "none",
-  color: "white",
-};
-
   return (
-    <footer style={style}>
-
-        <div className="container">
+    <footer className="style-Footer">
+      <div className="container">
             <div className="row">
                 <div className="col">
                     <h3>Davi React</h3>
-                    <button className="menu-a mt-1"><a href="https://github.com/1DaviSLima" style={linkStyle}>GitHub</a> </button>                          
+                    <button className="menu-a mt-1"><a href="https://github.com/1DaviSLima" className="link-Style">GitHub</a></button>                          
                     <p className="text-end">© 2026 Davi React</p>
                 </div>
             </div>
